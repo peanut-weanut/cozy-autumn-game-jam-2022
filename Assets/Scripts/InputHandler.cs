c1bdb19715dac090869b0d8ec23f9860160e85ee
peanut-weanut <109343736+peanut-weanut@users.noreply.github.com>
2022-09-12 21:33:17 -0700

    Fixing another error

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    // public static InputSystem controls = new InputSystem(); 
+    // private void OnEnable()
+    // {
+    //     InputHandler.controls.Enable();
+    // }
+
+    // private void OnDisable()
+    // {
+    //     InputHandler.controls.Disable();
+    // }
+}
